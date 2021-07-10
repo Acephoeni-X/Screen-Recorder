3b476cbbb1c28a243cb93b433297ad09053f6edb
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-07-10 20:32:19 +0530

    Delete Content.js

--- Content.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  
-  var link = document.getElementById('start');
-    
-  link.addEventListener('click', function() {
-      alert('start');
-    });
-});
