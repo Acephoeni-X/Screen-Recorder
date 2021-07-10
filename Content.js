3d25765930bcd013705358811b13c26fffeb225f
Rishi Sharma <rishisharma@Rishis-MacBook-Air.local>
2021-07-10 20:31:47 +0530

    update

---
+++ Content.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  var link = document.getElementById('start');
+    
+  link.addEventListener('click', function() {
+      alert('start');
+    });
+});
